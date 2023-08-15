4f45d0f53afd0a2fed7ef4274d92da40ce1a3ed0
Daniel Promise <promise.olayimika.171076@fuoye.edu.ng>
2023-08-15 06:59:46 +0100

    feat:model classes for DB

--- website/models.py
+++ website/models.py
@@ -49,16 +49,25 @@ class Lecturers(db.Model, UserMixin):
     def password(self, plain_text_password):
         self.password_hash = bcrypt.generate_password_hash(plain_text_password).decode('utf-8')
 
-    def check_password_correction(self, attempted_password):
+    def check_password_correctness(self, attempted_password):
         return bcrypt.check_password_hash(self.password_hash, attempted_password)
 
-class Students(db.Model):
+class Students(UserMixin, db.Model):
     matric_no = db.Column(db.String(15), primary_key=True)
     first_name = db.Column(db.String(150), nullable=False)
     last_name = db.Column(db.String(150), nullable=False)
     level_id = db.Column(db.Integer, db.ForeignKey('levels.id'))
     faculty_id = db.Column(db.Integer, db.ForeignKey('faculties.id'))
     department_id = db.Column(db.Integer, db.ForeignKey('departments.id'))
+    finger_id = db.Column(db.Integer, unique=True)
+
+    def check_first_name_correctness(self, submitted_name):
+        return self.first_name == submitted_name
+    
+    def get_id(self):
+           return (self.matric_no)
+    
+    enrolled_courses = db.relationship('Enrolled_courses')
 
 class Courses(db.Model):
     course_code = db.Column(db.String(10), primary_key=True)
@@ -67,3 +76,16 @@ class Courses(db.Model):
     department_id = db.Column(db.Integer, db.ForeignKey('departments.id'))
     level_id = db.Column(db.Integer, db.ForeignKey('levels.id'))
     staff_no = db.Column(db.String(20), db.ForeignKey('lecturers.id'))
+
+    enrolled_courses = db.relationship('Enrolled_courses')
+
+class Enrolled_courses(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    matric_no = db.Column(db.String(15), db.ForeignKey('students.matric_no'), nullable=False)
+    course_code = db.Column(db.String(15), db.ForeignKey('courses.course_code'), nullable=False)
+
+class attendance_base(db.Model):
+    __abstract__ = True
+    id = db.Column(db.Integer, primary_key=True)
+    matric_no = db.Column(db.String(15), nullable=False)
+    first_name = db.Column(db.String(150),nullable=False)
