d7bfe68813bd4cb2b338e1dc62f1fed3c8e5422a
Daniel Promise <promise.olayimika.171076@fuoye.edu.ng>
2023-08-24 06:48:27 +0100

    fix:dropdown not always responding

--- website/static/index.js
+++ website/static/index.js
@@ -1,34 +1,42 @@
-let levelData, facultyData = {}
+let levelData, facultyData = {};
 
-window.onload = facultiesAndDepartmentsjson()
-window.onload = leveljson()
+// window.onload = leveljson();
 
 async function facultiesAndDepartmentsjson() {
   const response = await fetch('/api/facs-and-depts');
-  facultyData = await response.json();
+  return await response.json();
 }
 
-async function leveljson() {
-  const response = await fetch('/api/levels');
-  levelData = await response.json();
-}
+// async function leveljson() {
+//   const response = await fetch('/api/levels');
+//   levelData = await response.json();
+// }
 
 function facultyList() {
-  let facultySelect = document.getElementById('faculty')
-  facultySelect.length = 0
-  let defaultOption = document.createElement('option')
-  defaultOption.text = 'Select your Faculty'
-  defaultOption.disabled = true
-  facultySelect.add(defaultOption)
-  facultySelect.selectedIndex = 0
+  let facultySelect = document.getElementById('faculty');
+  facultySelect.length = 0;
+  let defaultOption = document.createElement('option');
+  defaultOption.text = 'Select your Faculty';
+  defaultOption.disabled = true;
+  facultySelect.add(defaultOption);
+  facultySelect.selectedIndex = 0;
 
   for (let x in facultyData) {
-    let option = document.createElement('option')
-    option.text = x
-    option.value = facultyData[x][0]['id']
-    facultySelect.add(option)
+      let option = document.createElement('option');
+      option.text = x;
+      option.value = facultyData[x][0]['id'];
+      facultySelect.add(option);
   }
 }
+
+window.addEventListener("DOMContentLoaded", async (event) => {
+  try {
+      facultyData = await facultiesAndDepartmentsjson();
+      facultyList();
+  } catch (error) {
+      console.error('Error fetching data:', error);
+  }
+});
 
 function updateDepts(selected) {
   let departmentSelect = document.getElementById('department')
@@ -71,7 +79,6 @@ window.addEventListener("DOMContentLoaded", (event) => {
     .then(data => {
         // Handle the response from the backend
         document.getElementById("enroll-message").innerHTML = data.message
-        // You can display the fingerprint position in the frontend
         console.log("Fingerprint Position:", data.position);
     })
     .catch(error => {
